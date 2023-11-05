a1d1301a0525def9eb92a3ca11b2dcf39b048a5f
hoangvi <hoangvi0409@gmail.com>
2023-11-05 21:49:10 +0700

    fix

--- js/darklight/darklight.js
+++ js/darklight/darklight.js
@@ -10,9 +10,8 @@ Ngược lại, khi click vào nút sun ☀️, ta sẽ xóa class dark để cs
 //gọi thẻ button có id là switchButton
 // gắn chức năng click cho switchButton
 document.getElementById("switchButton").onclick = function () {
-    //khi button được click thì tìm đến thẻ body có id là myBody
-    // dùng classlist để gọi thuộc tính class của thẻ body
-    //Sử dụng toggle() để thêm hoặc xóa 1 class của thẻ html. Nếu thẻ body không có class dark thì toggle sẽ thêm class dark vào body. Ngược lại, nếu thẻ body đang có class dark thì toogle sẽ xóa class dark đi.
-    document.getElementById("myBody").classList.toggle("dark");
-  };
-  
+  //khi button được click thì tìm đến thẻ body có id là myBody
+  // dùng classlist để gọi thuộc tính class của thẻ body
+  //Sử dụng toggle() để thêm hoặc xóa 1 class của thẻ html. Nếu thẻ body không có class dark thì toggle sẽ thêm class dark vào body. Ngược lại, nếu thẻ body đang có class dark thì toogle sẽ xóa class dark đi.
+  document.getElementById("myBody").classList.toggle("dark");
+};
